bcb9f2f784e54c9376c588f95c4d702ace90d13f
”竹内幹人” <orzmktmkt310@gmail.com>
2019-08-31 13:26:47 +0000

    新規募集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,26 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  
   def new
     @post = Post.new
   end
 
   def create
     @post = current_user.posts.new(post_params)
-
+#binding.pry
     if @post.save
-      redirect_to posts_path, success: '投稿に成功しました'
+      redirect_to posts_path, success: '作成に成功しました'
     else
-      flash.now[:danger] = "投稿に失敗しました"
+      flash.now[:danger] = "作成に失敗しました"
       render :new
     end
   end
 
   private
   def post_params
-    params.require(:post).permit(:gelande_id, :date, :title, :text, :how)
+    params.require(:post).permit(:title, :gelande_id, :date, :how, :text)
   end
 end
